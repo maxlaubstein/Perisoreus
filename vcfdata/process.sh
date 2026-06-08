e91214f3d5e57c69041ae2b3ffd634bfa323c18b
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-06-08 15:16:06 -0400

    Update process.sh

--- vcfdata/process.sh
+++ vcfdata/process.sh
@@ -15,3 +15,24 @@ vcftools --gzvcf /media/maxlaubstein/22ea017e-93fd-4567-876e-3b9a92534a89/maxlau
 mv Perisoreus_clean.recode.vcf Perisoreus_clean.vcf
 bgzip Perisoreus_clean.vcf
 tabix -p vcf Perisoreus_clean.vcf.gz
+
+#Now, get a vcf just of the placed autosomal scaffolds
+#49 is mtdna, 47 W, 48 Z
+
+awk '{print $1}' \
+  /media/maxlaubstein/22ea017e-93fd-4567-876e-3b9a92534a89/maxlaubstein/Perisoreus/snp_calling_Perisoreus/results/reference/perCan.fa.gz.fai \
+  | grep CM \
+  | grep -v CM148749.1 \
+  | grep -v CM148747.1 \
+  | grep -v CM148748.1 \
+  | paste -sd, - 
+
+#CM148719.1,CM148720.1,CM148721.1,CM148722.1,CM148723.1,CM148724.1,CM148725.1,CM148726.1,CM148727.1,CM148728.1,CM148729.1,CM148730.1,CM148731.1,CM148732.1,CM148733.1,CM148734.1,CM148735.1,CM148736.1,CM148737.1,CM148738.1,CM148739.1,CM148740.1,CM148741.1,CM148742.1,CM148743.1,CM148744.1,CM148745.1,CM148746.1
+
+bcftools view -r CM148719.1,CM148720.1,CM148721.1,CM148722.1,CM148723.1,CM148724.1,CM148725.1,CM148726.1,CM148727.1,CM148728.1,CM148729.1,CM148730.1,CM148731.1,CM148732.1,CM148733.1,CM148734.1,CM148735.1,CM148736.1,CM148737.1,CM148738.1,CM148739.1,CM148740.1,CM148741.1,CM148742.1,CM148743.1,CM148744.1,CM148745.1,CM148746.1 \
+  Perisoreus_clean.vcf.gz \
+  -O z -o Perisoreus_clean_autosomes.vcf.gz
+
+
+
+
