f1afb7c47141729fa56d9e44bbefded00c3cd649
Max Laubstein <152578824+maxlaubstein@users.noreply.github.com>
2026-06-08 01:37:48 -0400

    Update pull_mtdna.sh

--- mtdna/pull_mtdna.sh
+++ mtdna/pull_mtdna.sh
@@ -4,8 +4,7 @@ cd /media/maxlaubstein/22ea017e-93fd-4567-876e-3b9a92534a89/maxlaubstein/Perisor
 samtools faidx /media/maxlaubstein/22ea017e-93fd-4567-876e-3b9a92534a89/maxlaubstein/Perisoreus/snp_calling_Perisoreus/results/reference/perCan.fa.gz CM148749.1 > mitogenome.fa
 
 #get raw vcf just from the mitochondrial scaffold
-vcftools --gzvcf /media/maxlaubstein/22ea017e-93fd-4567-876e-3b9a92534a89/maxlaubstein/Perisoreus/snp_calling_Perisoreus/results/vcfs/raw.vcf.gz \
-  --chr CM148749.1 \
-  --recode \
-  --recode-INFO-all \
-  --out Perisoreus_mtdna_RAW
+bcftools view \
+  -r CM148749.1 \
+  -Oz -o Perisoreus_mtdna_RAW.vcf.gz \
+  /media/maxlaubstein/22ea017e-93fd-4567-876e-3b9a92534a89/maxlaubstein/Perisoreus/snp_calling_Perisoreus/results/vcfs/raw.vcf.gz
